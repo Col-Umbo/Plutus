0497b7fcf48365050ce7285e0bc43f63d9767418
IceFireFish <icefirefish418@gmail.com>
2026-01-28 15:25:26 -0500

    Cleanup
    Cleaned up main.py, added requirements list and list of tasks to readme

--- main.py
+++ main.py
@@ -6,21 +6,15 @@ from PySide6.QtCore import QFileInfo
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
         # Create a QWebEngineView widget
         self.browser = QWebEngineView()
-
-        # Load a local HTML file or remote URL
-        # For a local file, ensure the URL is properly formatted:
-        # e.g., QUrl.fromLocalFile("/absolute/path/to/your/index.html")
+        
+        # In order to allow relative paths to work across different systems, follow the below example
         local_html = QUrl.fromLocalFile(QFileInfo("index.html").absoluteFilePath())
         self.browser.setUrl(local_html)
-        
-        # Alternatively, to load a remote URL:
-        # self.browser.setUrl(QUrl("https://www.google.com"))
 
         self.setCentralWidget(self.browser)
-        self.setWindowTitle("PySide6 HTML UI Example")
+        self.setWindowTitle("Plutus Budget Tracker")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
