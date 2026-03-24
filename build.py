5b1671ac99dfb437d9108490328fe1d6fe00ce8f
IceFireFish <icefirefish418@gmail.com>
2026-03-24 12:00:31 -0400

    Update build.py

--- build.py
+++ build.py
@@ -5,6 +5,6 @@ command = ['main.py', '--clean']
 for root, dirs, files in os.walk("."):
     if files:
         for file in files:
-            if (os.path.splitext(file)[1] == '.js' or os.path.splitext(file)[1]== '.html' or os.path.splitext(file)[1]=='.css' or os.path.splitext(file)[1] == '.png'):
+            if (os.path.splitext(file)[1] == '.js' or os.path.splitext(file)[1]== '.html' or os.path.splitext(file)[1]=='.css' or os.path.splitext(file)[1] == '.png' or os.path.splitext(file)[1]=='.ttf'):
                 command.append('--add-data='+os.path.join(root,file)+os.pathsep+root)
 pyinstaller.run(command)
