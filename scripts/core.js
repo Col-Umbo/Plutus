5f65f873deb0a3b013ea4e348910541eb7fca1e6
sonhoang002 <120953254+sonhoang002@users.noreply.github.com>
2026-04-01 00:12:38 -0400

    Add style to summary

--- scripts/core.js
+++ scripts/core.js
@@ -1,4 +1,3 @@
-// ==================== Utilities ====================
 const $ = (sel, root = document) => root.querySelector(sel);
 const $$ = (sel, root = document) => Array.from(root.querySelectorAll(sel));
 
